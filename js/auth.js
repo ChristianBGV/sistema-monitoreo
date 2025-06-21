0be39a8ec6758d635d40f2e0178ea9d1e1ea157a
ChristianBGV <christiangv.147@gmail.com>
2025-06-20 21:17:46 -0500

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,17 @@
-// Lógica de auth.js
+<script>
+  document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const email = document.querySelector('input[type="email"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+      localStorage.setItem('loggedInUser', JSON.stringify(user));
+      window.location.href = 'crear-evento.html'; // Panel de monitoreo
+    } else {
+      alert('Correo o contraseña incorrectos');
+    }
+  });
+</script>
